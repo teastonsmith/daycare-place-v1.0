b0e91a9809c4ac5ea3e78e84eb4310426d6dfc44
teastonsmith2 <t.easton.smith2@gmail.com>
2019-06-10 12:31:17 -0600

    register and login endpoints built and functional

---
+++ server/controllers/auth_controller.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+	register: async (req, res) => {
+		const { first_name, last_name, email, username, password } = req.body;
+		const db = req.app.get('db');
+		const { session } = req;
+		const userFound = await db.check_user_email({ email });
+		if (userFound[0]) return res.status(409).send('Email already exists');
+		const salt = bcrypt.genSaltSync(10);
+		const hash = bcrypt.hashSync(password, salt);
+		const createdUser = await db.register_user({
+			first_name,
+			last_name,
+			email,
+			username,
+			password: hash,
+		});
+		session.user = {
+			id: createdUser[0].login_id,
+			username: createdUser[0].username,
+		};
+		res.status(200).send(session.user);
+	},
+	login: async (req, res) => {
+		const { username, password } = req.body;
+		const db = req.app.get('db');
+		const { session } = req;
+		const userFound = await db.check_username({ username });
+		if (!userFound[0]) return res.status(401).send('User does not exist');
+		const authenticated = bcrypt.compareSync(password, userFound[0].password);
+		if (authenticated) {
+			session.user = {
+				id: userFound[0].login_id,
+				username: userFound[0].username,
+			};
+			res.status(200).send(session.user);
+		} else {
+			return res.status(401).send('Incorrect username or password');
+		}
+	},
+	// getDetails: async (req, res) => {},
+	// getUser: async (req, res) => {},
+	// logout: async (req, res) => {},
+};
